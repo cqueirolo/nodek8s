cb673203d708eb210f115b8e162ae0201686cb09
cqueirolo <cqueirolo@fi.uba.ar>
2022-11-22 07:24:01 -0300

    asd

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express()
 app.use(express.json())
@@ -10,6 +11,12 @@ app.get('/ping', (req, res) => {
         message: 'pong'
     })
 })
+
+app.get('/api-docs', async (req, res) => {
+    const response = await axios.get('https://fiuber-api-gateway.herokuapp.com/ping')
+    res.status(200).send(response.data)
+})
+
 
 app.listen(port, () => {
     console.log(`port: ${port}`)
